4a3c8c6c567fccb2be1348e973bd515569330011
quinzonjosh <quinzonjosh261@gmail.com>
2023-11-20 13:19:56 +0800

    initial setup

---
+++ main.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    n = int(input("Enter the number of slots inside the fitting room: "))
+    b = int(input("Enter the number of blue threads: "))
+    g = int(input("Enter the number of green threads: "))
+
+    
